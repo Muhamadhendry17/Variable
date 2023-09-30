0fb53f5b20e5d630b7974b9891db2b59fded5a97
hendryawnn <109840559+Muhamadhendry17@users.noreply.github.com>
2023-09-30 10:32:28 +0700

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,8 @@
+const firstName = "Muhamad";
+const lastName = "Hendry";
+const age = 18;
+const isMarried = false;
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(isMarried)
